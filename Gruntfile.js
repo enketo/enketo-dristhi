81683d3cc1eb68757c85254adfa77e5705f204d4
Martijn van de Rijdt <martijn@enketo.org>
2013-09-13 13:36:34 -0600

    added sass build task for grunt, closes #2, compiled sass with bootstrap, closes #6, removed last incorrect spelling of dristhi

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,7 +13,7 @@ module.exports = function(grunt) {
       dristhi: {
         files: {
           'build/js/webform-drishti-all-min.js': [
-            'lib/enketo-core/src/js/helpers.js', 
+            'lib/enketo-core/src/js/utils.js', 
             'src/js/gui.js', 
             'src/js/storage-dristhi.js',
             'lib/enketo-core/src/js/form.js', 
@@ -21,13 +21,23 @@ module.exports = function(grunt) {
             'src/js/webform-dristhi.js'
           ],
           'build/js/libraries-all-min.js': [
-            'lib/jquery.min.js', 
-            'lib/bootstrap.js', 
-            'lib/modernizr.min.js',
+            'lib/enketo-core/lib/jquery.min.js', 
+            'lib/enketo-core/lib/bootstrap.js', 
+            'lib/enketo-core/lib/modernizr.min.js',
             'lib/enketo-core/lib/bootstrap-timepicker/js/bootstrap-timepicker.js',
             'lib/enketo-core/lib/bootstrap-datepicker/js/bootstrap-datepicker.js', 
             'lib/enketo-core/lib/xpath/build/xpathjs_javarosa.min.js'
           ]
+        }
+      }
+    },
+    sass: {
+      dist: {
+        options: {
+          style: 'compact'
+        },
+        files: {
+          'build/css/dristhi.css': 'src/sass/dristhi.scss'
         }
       }
     },
@@ -38,9 +48,9 @@ module.exports = function(grunt) {
           specs: 'tests/spec/*.js',
           helpers: ['tests/utils/*.js', 'tests/mocks/*.js', 'build/mocks/*.js'],
           vendor: [
-            'lib/jquery.min.js', 
-            'lib/bootstrap.min.js',
-            'lib/modernizr.min.js',
+            'lib/enketo-core/lib/jquery.min.js', 
+            'lib/enketo-core/lib/bootstrap.js',
+            'lib/enketo-core/lib/modernizr.min.js',
             'lib/enketo-core/src/js/utils.js',
             'lib/enketo-core/lib/xpath/build/xpathjs_javarosa.min.js',
             'lib/enketo-core/lib/bootstrap-datepicker/js/bootstrap-datepicker.js',
@@ -54,8 +64,9 @@ module.exports = function(grunt) {
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-jasmine');
+  grunt.loadNpmTasks('grunt-contrib-sass');
 
   // Default task(s).
   grunt.registerTask('test', ['jasmine']);
-  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['uglify', 'sass', 'test']);
 };
